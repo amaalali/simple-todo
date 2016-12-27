a5d93e66a6bfec731c09757acbc3d56699893944
Amaal Ali <amq.ali@gmail.com>
2016-12-27 17:03:23 +0000

    Fix: Logged out user & Not owner can delete task.

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -33,7 +33,7 @@ Meteor.methods({
 	'tasks.remove'(taskId) {
 		check(taskId, String);
 		const task = Tasks.findOne(taskId);
-		if (task.private && task.owner !== this.userId) {
+		if (task.private || task.owner !== this.userId) {
 			throw new Meteor.Error('not-authorized');
 		}
 		Tasks.remove(taskId);
@@ -42,7 +42,7 @@ Meteor.methods({
 		check(taskId, String);
 		check(setChecked, Boolean);
 		const task = Tasks.findOne(taskId);
-		if (task.private && task.owner !== this.userId) {
+		if (task.private || task.owner !== this.userId) {
 			throw new Meteor.Error('not-authorized');
 		}
 		Tasks.update( taskId , { $set: { checked: setChecked } } );
